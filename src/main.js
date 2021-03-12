5be17131463e048c8581ef427ee64558be8460f6
WebbLu <u.master.o.twn@gmail.com>
2021-03-12 12:49:23 +0800

    init repo

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  vuetify,
+  components: { App },
+  template: '<App/>',
+});
